831baf883e7b5bc6d9351875c1c0578e18eed26e
Tomasz Pajor <tomek@polishgeeks.com>
2019-01-19 11:24:24 +0100

    fix Style/ExpandPathArguments

--- example/config/boot.rb
+++ example/config/boot.rb
@@ -2,7 +2,7 @@
 
 # Defines our constants
 RACK_ENV = ENV['RACK_ENV'] ||= 'development'  unless defined?(RACK_ENV)
-PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
+PADRINO_ROOT = File.expand_path('..', __dir__) unless defined?(PADRINO_ROOT)
 
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
